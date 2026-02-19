24bab270d7e742e67a9241a21e8c3c0929e8f073
riznyk.serhii.v@gmail.com <riznyk.serhii.v@gmail.com>
2026-02-19 10:08:22 +0100

    feat(backend): add Netlify URL to CORS origins

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,7 +7,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:5173', 'http://localhost:3000'],
+    origin: [
+      'http://localhost:5173',
+      'http://localhost:3000',
+      'https://zen-bit.netlify.app',
+    ],
     credentials: true,
   });
 
